a02423cba4e7ca37ff7d1339520337638ff6197f
M0Hassan <aminmohamedadan@gmail.com>
2020-09-27 19:30:02 +0300

    Third commit

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -37,7 +37,8 @@ class AppAsset extends AssetBundle
         'theme/assets/js/main.js'
     ];
     public $depends = [
-        'yii\web\YiiAsset',
+    /*    'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        */
     ];
 }
